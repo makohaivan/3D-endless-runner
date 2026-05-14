9319fa0c9a3431b954a9da1ab83d5d724cac5e51
makohaivan <makohaivan26@gmai.com>
2026-05-14 08:15:44 +0300

    added more materials and player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 2;
+    public float horizontalSpeed = 3;
+
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
+
+    void Update()
+    {
+        // Forward movement
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+
+        // Horizontal movement 
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed, Space.World);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed, Space.World);
+            }
+        }
+
+    }
+}
